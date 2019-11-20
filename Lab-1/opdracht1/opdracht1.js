df947ceedfc20db1766498833b47b49b4a0fc44f
Swen <99058148@mydavinci.nl>
2019-11-20 12:13:00 +0100

    Commit1

---
+++ Lab-1/opdracht1/opdracht1.js
@@ -0,0 +1,23 @@
+var alledagen =["Alle dagen van de week zijn: <br> Maandag ", " Dinsdag", " Woensdag", " Donderdag", " Vrijdag", " Zaterdag", " Zondag"]
+var lijst = document.getElementById('dagen');
+lijst.innerHTML= alledagen;
+
+var werkdagen=["Alle werkdagen zijn: <br>Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag"];
+var lijst = document.getElementById('werkdagen');
+lijst.innerHTML= werkdagen;
+
+var weekenddagen =["De weekenddagen zijn: <br>Zaterdag","Zondag"];
+var lijst = document.getElementById('weekend');
+lijst.innerHTML=weekenddagen
+
+var omgekeerdedagen= ["Alle dagen van de week in omgekeerde volgorde zijn: <br>Zondag","Zaterdag","Vrijdag","Donderdag","Woensdag","Dinsdag","Maandag"]
+var lijst = document.getElementById('omgekeerd');
+lijst.innerHTML=omgekeerdedagen
+
+var omgekeerdewerkdagen= [" De werkdagen in omgekeerde volgorde zijn: <br>Vrijdag","Donderdag","Woensdag","Dinsdag","Maandag"];
+var lijst = document.getElementById('omgekeerdewerkdagen');
+lijst.innerHTML=omgekeerdewerkdagen
+
+var weekendomgekeerd=["De weekenddagen in omgekeerde volgorde zijn: <br>Zondag","Zaterdag"];
+var lijst = document.getElementById('omgekeerdweekend');
+lijst.innerHTML=weekendomgekeerd
---
+++ Lab-1/opdracht1/opdracht2.js
@@ -0,0 +1,9 @@
+var hoeveelnamen = prompt("Hoeveel namen wilt u in de array stoppen? (Minimaal 3)")
+if(hoeveelnamen >=3){
+	var welkenamen=prompt("Welke namen wilt u toevoegen?")
+}
+
+
+else if(hoeveelnamen <3){
+	alert("U moet minimaal 3 namen invullen")
+}
